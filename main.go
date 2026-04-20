7705fea0529e4f9e57e3467d58f8a0509f98ad5f
Ayomide-sotayo <shotayoolumide55@gmail.com>
2026-04-20 14:40:44 +0100

    deploy

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
-
 	// Week 1 routes (kept exactly as before)
 	r.HandleFunc("/checkins", GetCheckins).Methods("GET")
 	r.HandleFunc("/checkins", CreateCheckin).Methods("POST")
@@ -18,6 +19,10 @@ func main() {
 	// Week 2 new route
 	r.HandleFunc("/checkins/{id}", GetCheckinByID).Methods("GET")
 
-	fmt.Println("Server is running on :8080...")
-	http.ListenAndServe(":8080", r)
+	port := os.Getenv("PORT")
+if port == "" {
+    port = "8080"
+}
+fmt.Println("Server is running on :" + port + "...")
+http.ListenAndServe(":"+port, r)
 }
